0bc1c14aa9025dbc80a01e0a8cdd1ec833fab162
Agutale-Arpit <arpitagutale947@gmail.com>
2025-03-09 17:05:24 +0530

    Initial Commit

---
+++ src/components/LoginPage.tsx
@@ -0,0 +1,14 @@
+import SignIn from "./sign-in";
+
+function LoginPage() {
+  return (
+    <div className="flex">
+      <div>Login Page</div>
+      <div>
+        <SignIn />
+      </div>
+    </div>
+  )
+}
+
+export default LoginPage
