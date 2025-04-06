5f7f8dddc5e3bad52641a66591a0f7612780ab81
Agutale-Arpit <arpitagutale947@gmail.com>
2025-04-06 16:52:57 +0530

    Image Error Resolved

--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -4,14 +4,14 @@ import { auth } from "../../auth";
 export default async function UserAvatar() {
   const session = await auth();
   console.log(session);
-
+  const fallbackImage = "../../public/file.svg";
   if (!session?.user) return null
 
   return (
     <div>
       <Image
         className="rounded-full"
-        src={session.user.image} width={40} height={40} alt="User Avatar" />
+        src={session.user.image ?? fallbackImage} width={40} height={40} alt="User Avatar" />
     </div>
   )
 }
